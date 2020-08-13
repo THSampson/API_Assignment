34980366fd661e6cc3149ebcf6f78c915830d4c9
Taylor Sampson <thsampsn@gmail.com>
2020-08-13 13:48:39 -0400

    updating files

--- pokes.js
+++ pokes.js
@@ -1,1 +1,21 @@
-console.log("I wanna be the very best");
+const url = "https://pokeapi.co/api/v2/region/";
+
+
+let pokeTypeList = document.querySelector('ul');
+
+fetch(url) 
+.then(function(results) {
+    return results.json();
+})
+.then(function(json) {
+let type = json.results;
+    for (t of type) {
+        let typeList = document.createElement('li');
+        typeList.innerHTML = '<p>' + t.name + '</p>';
+        pokeTypeList.appendChild(typeList);
+    }
+ })
+ .catch(function(err) {
+     console.log(err);
+ });
+
